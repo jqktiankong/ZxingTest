95a4b7829ac907d7124046c6889da6ad49501b58
jiqingke <jiqingke>
2018-07-12 08:49:27 +0800

    规避掉第一次申请权限时使用识别相册中的二维码返回出现黑屏的bug

--- app/src/main/java/com/jqk/zxingtest/MainActivity.java
+++ app/src/main/java/com/jqk/zxingtest/MainActivity.java
@@ -1,8 +1,16 @@
 package com.jqk.zxingtest;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -22,7 +30,31 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         scan = (Button) findViewById(R.id.scan);
 
         scan.setOnClickListener(this);
+
+        //需要在外部请求摄像头权限，否则功能会有影响
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            requestPermission();
+        }
     }
+
+    private void requestPermission() {
+        //判断是否已经赋予权限
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+            //如果应用之前请求过此权限但用户拒绝了请求，此方法将返回 true。
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.CAMERA)) {//这里可以写个对话框之类的项向用户解释为什么要申请权限，并在对话框的确认键后续再次申请权限
+            } else {
+                //申请权限，字符串数组内是一个或多个要申请的权限，1是申请权限结果的返回参数，在onRequestPermissionsResult可以得知申请结果
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.CAMERA,}, 1);
+            }
+        } else {
+
+        }
+    }
+
 
     @Override
     public void onClick(View v) {
@@ -42,4 +74,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             Toast.makeText(this, data.getCharSequenceExtra("scanResult"), Toast.LENGTH_SHORT).show();
         }
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                } else {
+                    finish();
+                }
+                return;
+            }
+        }
+    }
+
 }
--- zxing/src/main/java/com/google/zxing/client/android/CaptureActivity.java
+++ zxing/src/main/java/com/google/zxing/client/android/CaptureActivity.java
@@ -278,73 +278,16 @@ public final class CaptureActivity extends Activity implements SurfaceHolder.Cal
             characterSet = intent.getStringExtra(Intents.Scan.CHARACTER_SET);
         }
 
-        // 动态申请摄像头权限
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            requestPermission();
+        SurfaceView surfaceView = (SurfaceView) findViewById(R.id.preview_view);
+        SurfaceHolder surfaceHolder = surfaceView.getHolder();
+        if (hasSurface) {
+            // The activity was paused but not stopped, so the surface still exists. Therefore
+            // surfaceCreated() won't be called, so init the camera here.
+            initCamera(surfaceHolder);
         } else {
-            SurfaceView surfaceView = (SurfaceView) findViewById(R.id.preview_view);
-            SurfaceHolder surfaceHolder = surfaceView.getHolder();
-            if (hasSurface) {
-                // The activity was paused but not stopped, so the surface still exists. Therefore
-                // surfaceCreated() won't be called, so init the camera here.
-                initCamera(surfaceHolder);
-            } else {
-                // Install the callback and wait for surfaceCreated() to init the camera.
-                Log.d("123", "addCallback");
-                surfaceHolder.addCallback(this);
-            }
-        }
-    }
-
-    /*
-   申请摄像头权限
-    */
-    private void requestPermission() {
-        //判断是否已经赋予权限
-        if (ContextCompat.checkSelfPermission(this,
-                Manifest.permission.CAMERA)
-                != PackageManager.PERMISSION_GRANTED) {
-            //如果应用之前请求过此权限但用户拒绝了请求，此方法将返回 true。
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
-                    Manifest.permission.CAMERA)) {//这里可以写个对话框之类的项向用户解释为什么要申请权限，并在对话框的确认键后续再次申请权限
-            } else {
-                //申请权限，字符串数组内是一个或多个要申请的权限，1是申请权限结果的返回参数，在onRequestPermissionsResult可以得知申请结果
-                ActivityCompat.requestPermissions(this,
-                        new String[]{Manifest.permission.CAMERA,}, 1);
-            }
-        } else {
-            SurfaceView surfaceView = (SurfaceView) findViewById(R.id.preview_view);
-            SurfaceHolder surfaceHolder = surfaceView.getHolder();
-            if (hasSurface) {
-                // The activity was paused but not stopped, so the surface still exists. Therefore
-                // surfaceCreated() won't be called, so init the camera here.
-                initCamera(surfaceHolder);
-            } else {
-                // Install the callback and wait for surfaceCreated() to init the camera.
-                Log.d("123", "addCallback");
-                surfaceHolder.addCallback(this);
-            }
-        }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           String permissions[], int[] grantResults) {
-        switch (requestCode) {
-            case 1: {
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    // 开启摄像头
-                    SurfaceView surfaceView = (SurfaceView) findViewById(R.id.preview_view);
-                    SurfaceHolder surfaceHolder = surfaceView.getHolder();
-                    initCamera(surfaceHolder);
-                    hasSurface = true;
-                } else {
-                    finish();
-                }
-                return;
-            }
+            // Install the callback and wait for surfaceCreated() to init the camera.
+            Log.d("123", "addCallback");
+            surfaceHolder.addCallback(this);
         }
     }
 
@@ -491,10 +434,6 @@ public final class CaptureActivity extends Activity implements SurfaceHolder.Cal
             Uri uri = intent.getData();
             decodeOrStoreSavedBitmap(decodeUriAsBitmap(uri), null);
         }
-        SurfaceView surfaceView = (SurfaceView) findViewById(R.id.preview_view);
-        SurfaceHolder surfaceHolder = surfaceView.getHolder();
-        initCamera(surfaceHolder);
-        hasSurface = true;
     }
 
     private Bitmap decodeUriAsBitmap(Uri uri) {
